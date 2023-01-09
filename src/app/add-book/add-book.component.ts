14bc5b8602ec2f5e241dcff5ec47dbe783b5e64d
yashprajapati26 <yash260801@gmail.com>
2023-01-09 14:38:57 +0530

    use template driven form instant of reactive form

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl,Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AddBookService } from './add-book.service';
 
@@ -12,14 +12,6 @@ export class AddBookComponent {
 
   constructor(private addBookService: AddBookService, private router:Router){};
 
-  bookForm = new FormGroup({
-    title: new FormControl('',[Validators.required]),
-    price: new FormControl('',[Validators.required]),
-    // book_image: new FormControl('',[Validators.required]),
-    description: new FormControl('',[Validators.required]),
-    // file: new FormControl('', [Validators.required]),
-  });
-
   file:any;
   title:any;
   
@@ -28,18 +20,17 @@ export class AddBookComponent {
 
   ngOnInit() {};
 
-  addBook():void{
-    console.log(this.bookForm.value);
-     
-    // this.formData.append('file', this.file);
-    // this.formData.append('title', this.bookForm.get('title').value);
-    // this.formData.append('price', this.bookForm.get('price').value);
-    // this.formData.append('book_image', this.bookForm.get('book_image').value);
-    // this.formData.append('description', this.bookForm.get('description').value);
+  addBook(data:any):void{
+    
+    console.log("data :>",data)
 
-    
-    
-    this.addBookService.add_Book(this.bookForm.value).subscribe((result)=>{
+    this.formData.append('title', data['title']);
+    this.formData.append('price', data['price']);
+    this.formData.append('book_image', data['book_image']);
+    this.formData.append('description', data['description']);
+
+  
+    this.addBookService.add_Book(this.formData).subscribe((result)=>{
       if(result){
         console.log(result);
         this.router.navigate(['seller_home'])
@@ -50,26 +41,13 @@ export class AddBookComponent {
   
   onFileSelect(event:any):void{
     console.log('file selected',event);
-     
     this.file = event.target.files[0];
-    // this.bookForm.patchValue({
-    //   file:this.file
-    // })
-    
+    this.formData.append('file', this.file);
     console.log("select file ..",this.file);
-
-    this.addBookService.add_Book(this.formData).subscribe((result)=>{
-      if(result){
-        console.log(result);
-        this.router.navigate(['seller_home'])
-      }
-    })
-    // this.bookForm.patchValue({book_image:file});
-    const allowedMimeTypes = ["image/jpeg", "image/jpg","image/png"];
-
-   
-    console.log("888",this.bookForm.value);
-
-    
+    // const allowedMimeTypes = ["image/jpeg", "image/jpg","image/png"];
   }
 }
+
+
+
+
